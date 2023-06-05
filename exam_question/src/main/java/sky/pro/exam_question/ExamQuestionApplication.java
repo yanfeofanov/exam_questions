e8a1f7e2d3345abf88438dc92bc2ee5bd323bdae
kaVII-x <ka52jiguli@yandex.ru>
2023-06-05 19:34:04 +0400

    first start

---
+++ exam_question/src/main/java/sky/pro/exam_question/ExamQuestionApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.exam_question;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamQuestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamQuestionApplication.class, args);
+	}
+
+}
